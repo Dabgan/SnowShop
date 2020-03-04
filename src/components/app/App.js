7e76ffdb7a5a3cdcea52ffcc6b78952e1b9e7493
dabgan <gabriel.daniluk.work@gmail.com>
2020-03-04 21:01:39 +0100

    feat: add bootstrap by npm, add navbar and logo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./styles/App.css";
+import "./App.css";
+import Header from "../header/Header";
 
 function App() {
     return (
         <div>
-            <h1>Snowshop</h1>
+            <Header />
         </div>
     );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import logo from "./../../assets/images/logo.png";
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/#">
+                    <img src={logo} alt="logo" width="150px" />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a href="/#" className="nav-link">
+                                About Us
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/#" className="nav-link">
+                                About Us
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/#" className="nav-link">
+                                About Us
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/#" className="nav-link">
+                                About Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
