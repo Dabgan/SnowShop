da9e53b50604c0b620b5b76ce37ace21b92d67a4
dabgan <gabriel.daniluk.work@gmail.com>
2020-03-10 21:00:11 +0100

    feat: add newsletter component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "../header/Header";
+import Newsletter from "../newsletter/Newsletter";
 
 function App() {
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
+            <Newsletter />
         </div>
     );
 }
