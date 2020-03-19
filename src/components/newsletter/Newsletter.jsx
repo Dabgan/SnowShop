26335b23018843848fbc01f67ce71f6f7f689f4d
dabgan <gabriel.daniluk.work@gmail.com>
2020-03-19 19:18:33 +0100

    fix: add RWD to newsletter component

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -5,15 +5,20 @@ function Newsletter() {
     return (
         <div className="newsletter-container">
             <form className="newsletter-form">
-                <p className="h2">How about 10% off to your next order?</p>
-                <p>
-                    All you need to do, is to sing to our newsletter and get
-                    sweet 10% off to your next order! It's so simple!{" "}
+                <p className="newsletter-header">
+                    How about <span className="text-gradient">10%</span> off to
+                    your next order?
                 </p>
+                <div className="newsletter-description">
+                    <p>
+                        All you need to do, is to sing to our newsletter and get
+                        sweet 10% off to your next order! It's so simple!{" "}
+                    </p>
+                </div>
                 <div className="newsletter-input-container">
                     <input type="text" placeholder="Type your email" />
                 </div>
-                <button type="submit" className="my-btn">
+                <button type="submit" className="my-btn newsletter-btn">
                     Sign me!
                 </button>
             </form>
