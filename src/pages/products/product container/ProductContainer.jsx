bcbe854e446ec7de02cdc67640224fca8d7abe7e
dabgan <gabriel.daniluk.work@gmail.com>
2020-03-29 19:57:51 +0200

    feat: add photo preview (in modal)

--- src/pages/products/product container/ProductContainer.jsx
+++ src/pages/products/product container/ProductContainer.jsx
@@ -7,11 +7,37 @@ import photo from "../../../assets/images/products/goggles2.jpg";
 const ProductContainer = () => {
     return (
         <div className="product-container row">
-            <div className="product-photo-container col-md-6">
+            <div
+                className="product-photo-container col-md-6"
+                type="button"
+                data-toggle="modal"
+                data-target="#exampleModal"
+            >
                 <img src={photo} alt="a product" />
             </div>
             <ProductProperties />
             <ProductDescription />
+            {/* Modal */}
+            <div
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div
+                    className="modal-dialog modal-dialog-centered"
+                    role="document"
+                >
+                    <div className="modal-content">
+                        <div className="modal-body">
+                            <img src={photo} alt="a product" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* End of Modal */}
         </div>
     );
 };
