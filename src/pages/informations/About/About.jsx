75a42f0c2a6fec15be3c5a674d40b81093bf636d
dabgan <gabriel.daniluk.work@gmail.com>
2020-03-31 19:03:55 +0200

    fix: add about page content

--- src/pages/informations/About/About.jsx
+++ src/pages/informations/About/About.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import "./about.css";
+import Newsletter from "../../../components/newsletter/Newsletter";
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>
-                lorem impste All you need to do, is to sing to our newsletter
-                and get sweet 10% off to your next order! It's so simple!
-            </p>
-        </div>
+        <>
+            <div className="information-page-container">
+                <h1 className="h3 mb-4">About us</h1>
+                <p>
+                    The main idea of creating this webshop site was to practice
+                    the basics of React and learn React Hooks. In order to do
+                    that I wanted to create a place where I would like to buy
+                    myself. Where shopping is easy and enjoyable.
+                </p>
+                <h2 className="h3 my-4">Some more</h2>
+                <p>
+                    I've learned a lot by creating this site. First of all,
+                    perseverance in creating the project and how much time the
+                    details can take. I learned what React Hook's are and how to
+                    use them. I also learned how to use Firebase and how to use
+                    the database more smoothly.
+                </p>
+                <h2 className="h3 my-4">And more</h2>
+                <p>
+                    Propably not worth reading, I will put here some lorem ipsum
+                    to lengthen this paragraph. Snowboarding is a recreational
+                    activity and Winter Olympic and Paralympic sport that
+                    involves descending a snow-covered slope while standing on a
+                    snowboard attached to a rider's feet.
+                </p>
+            </div>
+            <Newsletter />
+        </>
     );
 };
 
