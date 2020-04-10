55965ba080ab858ad4a1cdb7fe8cb8565f1196d9
dabgan <gabriel.daniluk.work@gmail.com>
2020-04-10 10:24:11 +0200

    feat: add function that shows only unique marks of selected category

--- src/pages/categories/filters/FiltersContainer.jsx
+++ src/pages/categories/filters/FiltersContainer.jsx
@@ -7,35 +7,37 @@ function FiltersContainer({ name }) {
     const productsOfThisCategory = products.filter((product) => {
         return product.category === name;
     });
-    const marks = [
-        { name: "pulsar", id: 0 },
-        { name: "eagle slash", id: 1 },
-        { name: "hiperx", id: 2 },
-    ];
-    const newMarks = productsOfThisCategory.map((product) => ({
+
+    const marksInCategory = productsOfThisCategory.map((product) => ({
         name: product.mark,
         id: product.id,
     }));
+
+    const removeDuplicates = (originalArray, key) => {
+        return [
+            ...new Map(originalArray.map((item) => [item[key], item])).values(),
+        ];
+    };
+    const uniqueMarks = removeDuplicates(marksInCategory, "name");
+
     const prices = [
         { name: "$0-99", id: 0 },
         { name: "$99-199", id: 1 },
         { name: "$199-299", id: 2 },
     ];
 
-    console.log(newMarks);
+    console.log(uniqueMarks);
     return (
         <aside className="filter-container">
             <div className="block category-name">{name}</div>
             <div className="block filters">
                 Filtr by:
-                <FilterComponent filterName="mark" filterLabels={newMarks} />
+                <FilterComponent filterName="mark" filterLabels={uniqueMarks} />
                 <FilterComponent filterName="price" filterLabels={prices} />
                 <button className="my-btn">Clear filters</button>
             </div>
         </aside>
     );
 }
-
-// , "eagle slash", "hiperx"
 
 export default FiltersContainer;
