b9aa53326c02ed42e5f5a387994e46f3341f20df
dabgan <gabriel.danilukk@gmail.com>
2021-03-23 19:57:49 +0100

    fix: edit RWD styles for categories and products

--- src/components/header/nav bar/Navbar.jsx
+++ src/components/header/nav bar/Navbar.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import Burger from "./Burger";
+import React from 'react';
+import styled from 'styled-components';
+import Burger from './Burger';
 
 const Nav = styled.nav`
-    /* border: 2px solid #069cc8; */
-    width: 100%;
+    max-width: 100%;
     height: 55px;
     padding: 0;
     display: flex;
